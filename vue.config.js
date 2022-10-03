dd368ac333709090667dcda4e67f63475c0513c4
dai0618 <daidai0618@keio.jp>
2022-10-04 02:47:03 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
   outputDir: 'docs',
   assetsDir: './',
   publicPath: './',
-}
+});
